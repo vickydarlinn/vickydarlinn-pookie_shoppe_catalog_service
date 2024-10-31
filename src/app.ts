cac60b4e1c176f809a69867723b5fbea7b1af479
vickydarlinn <uttapalsangwan@gmail.com>
2024-10-31 11:24:38 +0530

    feat:add cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
+
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 import categoryRouter from "./category/category-router";
 import cookieParser from "cookie-parser";
+import { Config } from "./common/config";
 
 const app = express();
+const ALLOWED_DOMAINS = [
+  Config.CLIENT_UI_DOMAIN,
+  Config.ADMIN_UI_DOMAIN,
+  "http://localhost:5174"
+];
+app.use(
+  cors({
+    origin: ALLOWED_DOMAINS as string[],
+    credentials: true
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
--- src/common/config/index.ts
+++ src/common/config/index.ts
@@ -5,12 +5,22 @@ config({
   path: path.join(__dirname, `../../../.env.${process.env.NODE_ENV}`)
 });
 
-const { PORT, LOG_LEVEL, NODE_ENV, DB_URL, JWKS_URI } = process.env;
+const {
+  PORT,
+  LOG_LEVEL,
+  NODE_ENV,
+  DB_URL,
+  JWKS_URI,
+  CLIENT_UI_DOMAIN,
+  ADMIN_UI_DOMAIN
+} = process.env;
 
 export const Config = {
   PORT,
   LOG_LEVEL,
   NODE_ENV,
   DB_URL,
-  JWKS_URI
+  JWKS_URI,
+  CLIENT_UI_DOMAIN,
+  ADMIN_UI_DOMAIN
 };
